b42dc4f72fd8f2b640b5130bc09490d2d9d93d5a
Colin Forbes <cjforbes32@gmail.com>
2022-01-21 16:02:07 -0500

    finished section 2 of learndot lab

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const morgan = require("morgan");
+const { db, Page, User } = require("./models");
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
@@ -12,7 +17,15 @@ app.get("/", (req, res) => {
   console.log("hello");
   res.send("hello");
 });
-const PORT = 1338;
-app.listen(PORT, () => {
-  console.log("hello 1337");
-});
+
+const init = async () => {
+  // await Page.sync();
+  // await User.sync();
+  await db.sync({ force: true });
+  const PORT = 3000;
+  app.listen(PORT, () => {
+    console.log(`now listening at Port: ${PORT}`);
+  });
+};
+
+init();
