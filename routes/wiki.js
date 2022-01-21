47ef7aa42c94b24730a6e17484dd628f42278476
Sulaiman Marey <adammarey51@gmail.com>
2022-01-21 15:58:45 -0700

    the post action

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,6 +2,21 @@ const express = require("express");
 const res = require("express/lib/response");
 const wikiRouter = express.Router();
 const { addPage } = require("../views");
+const { Page } = require("../models");
+const { addPage } = require("../views");
+const { Router } = require("express");
+
+wikiRouter.post('/', async (req, res, next) => {
+  try {
+    const page = await Page.create({
+      title: 
+      content: 
+    })
+    res.redirect('/')
+  } catch (error) {
+    next(error)
+  }
+})
 
 wikiRouter.get("/", (req, res) => {
   res.send("WIKI");
